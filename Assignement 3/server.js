4355a8b26bcc5b963dac4c8084b3aa3a1a3ca03e
imdmahajankanika <48632517+imdmahajankanika@users.noreply.github.com>
2021-03-22 16:04:41 +0100

    Added changes

---
+++ Assignement 3/server.js
@@ -0,0 +1,11 @@
+const port = process.env.PORT||3000;
+const express = require('express');
+const app =express();
+
+app.use('/quiz',express.static("public"))
+app.get('/', function(req,res){
+    res.send("Main Page!")
+})
+app.listen(port,function(){
+    console.log("Server is listening at default port! "+port)
+})
